0cf6a06148ed64444d50dfeafb01a0f1ab9a558c
AchRizalAdi <adijr979@gmail.com>
2026-05-07 19:14:12 +0700

    back login

--- oakglobal_erp_custom/hooks.py
+++ oakglobal_erp_custom/hooks.py
@@ -6,9 +6,9 @@ app_email = "admin@oakglobalmaritim.com"
 app_license = "MIT"
 
 # Website assets. Keep ERPNext core untouched.
-web_include_css = [
-    "/assets/oakglobal_erp_custom/css/oakglobal_login.css",
-]
+# web_include_css = [
+#     "/assets/oakglobal_erp_custom/css/oakglobal_login.css",
+# ]
 
 # Fixtures can be added later to version-control UI customizations.
 fixtures = []
